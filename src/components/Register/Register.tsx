2379594753005151cbf34943cf7e1592a4b5ea56
Andrea Martinez <andrea.maort@gmail.com>
2022-08-20 20:45:13 +0200

    added style to index

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -5,31 +5,29 @@ export const Register = () => {
     <div className="main-component">
       <h2>Create your user</h2>
       <form>
-        <div>
-          <label htmlFor="userName">Enter your User Name</label>
-          <input
-            type="text"
-            name="userName"
-            placeholder="User name"
-            required
-            autoComplete="off"
-          />
+        <label htmlFor="userName">Enter your User Name</label>
+        <input
+          type="text"
+          name="userName"
+          placeholder="User name"
+          required
+          autoComplete="off"
+        />
 
-          <label htmlFor="password">Enter your password</label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            required
-            autoComplete="off"
-          />
-          <label htmlFor="userName">Enter your birthdate</label>
-          <input type="date" name="birthdate" required autoComplete="off" />
-          <label htmlFor="imageInput">Select an image</label>
-          <input type="file" name="imageInput" placeholder="image" />
+        <label htmlFor="password">Enter your password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+          autoComplete="off"
+        />
+        <label htmlFor="userName">Enter your birthdate</label>
+        <input type="date" name="birthdate" required autoComplete="off" />
+        <label htmlFor="imageInput">Select an image</label>
+        <input type="file" name="imageInput" placeholder="image" />
 
-          <button type="submit">Register</button>
-        </div>
+        <button type="submit">Register</button>
       </form>
     </div>
   );
